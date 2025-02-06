9f65c7731e6299e057ee68dfd4d4ea5f4e0ed853
Anton Lavrevov <lavrton@gmail.com>
2025-02-06 07:10:34 -0600

    fix build

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // experimental: {
+  //   turbo: {
+  //     resolveAlias: {
+  //       canvas: { browser: './empty.js' },
+  //     },
+  //   },
+  // },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    return config;
+  },
+};
+
+export default nextConfig;
