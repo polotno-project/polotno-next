c919ea4f071bab4188e3f745a94bc80fc2215302
Anton Lavrevov <lavrton@gmail.com>
2025-02-06 07:11:16 -0600

    restore

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,10 @@ const nextConfig = {
   //     },
   //   },
   // },
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+  //   return config;
+  // },
 };
 
 export default nextConfig;
