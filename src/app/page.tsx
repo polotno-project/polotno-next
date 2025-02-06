dd32f31f962fd0e4a8a33060d053f67da0377dd7
Anton Lavrevov <lavrton@gmail.com>
2025-02-06 07:19:26 -0600

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+// we MUST load the editor dynamically, otherwise server-side rendering will fail
 const Editor = dynamic(() => import('../components/editor'), {
   ssr: false,
 });
