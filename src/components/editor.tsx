547e60877005dbd2b82d5621b1e0b7b41341dcea
Anton Lavrevov <lavrton@gmail.com>
2025-02-06 07:00:30 -0600

    try react 19

---
+++ src/components/editor.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { PagesTimeline } from 'polotno/pages-timeline';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { SidePanel } from 'polotno/side-panel';
+import { Workspace } from 'polotno/canvas/workspace';
+
+import { createStore } from 'polotno/model/store';
+
+const store = createStore({
+  key: 'nFA5H9elEytDyPyvKL7T', // you can create it here: https://polotno.com/cabinet/
+  // you can hide back-link on a paid license
+  // but it will be good if you can keep it for Polotno project support
+  showCredit: true,
+});
+const page = store.addPage();
+
+export const Editor = () => {
+  return (
+    <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@blueprintjs/core@latest/lib/css/blueprint.css"
+      />
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+        <PagesTimeline store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
+  );
+};
+
+export default Editor;
