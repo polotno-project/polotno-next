6d6011929b010145309cbd56ecf9dc482b3bfa86
Anton Lavrevov <lavrton@gmail.com>
2025-04-22 09:46:31 -0500

    update deps, update docs, update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   turbo: {
-  //     resolveAlias: {
-  //       canvas: { browser: './empty.js' },
-  //     },
-  //   },
-  // },
-  // webpack: (config) => {
-  //   config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
-  //   return config;
-  // },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
